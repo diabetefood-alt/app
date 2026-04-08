102c7c1be7eda0d50ce766138135c4b64d24339e
diabetefood-alt <diabetefood@gmail.com>
2026-04-09 00:11:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js');
 
-const CACHE_NAME = 'diabetefood-v80';
+const CACHE_NAME = 'diabetefood-v81';
 const ASSETS = [
   '/app/',
   '/app/index.html',
